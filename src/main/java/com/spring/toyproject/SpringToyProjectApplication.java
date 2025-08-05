0d8b2a0bc2686840c79f7b24cf00af518765320f
Sirosho <pack8447@gmail.com>
2025-08-05 09:53:30 +0900

    기본파일들 생성

--- src/main/java/com/spring/toyproject/SpringToyProjectApplication.java
+++ src/main/java/com/spring/toyproject/SpringToyProjectApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringToyProjectApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(SpringToyProjectApplication.class, args);
-    }
 
+        SpringApplication.run(SpringToyProjectApplication.class, args);
+
+    }
 }
