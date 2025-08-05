af40cfec18f08fceddb840943592215cadd9c7a7
Sirosho <pack8447@gmail.com>
2025-08-05 10:31:02 +0900

    엔터티 및 레포지토리 생성

---
+++ src/main/java/com/spring/toyproject/repository/UserRepository.java
@@ -0,0 +1,26 @@
+package com.spring.toyproject.repository;
+
+
+import com.spring.toyproject.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    /*
+    *
+    *   로그인할 떄 사용자는 자신의 이름이나 이메일을 입력하지 숫자id를 입력하지 않음
+    *   그 이름이나 이메일을 통해 회원정보를 조회해야 한다.
+    *
+    * */
+
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+
+    // 중복확인
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+
+
+}
